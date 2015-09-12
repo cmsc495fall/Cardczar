ed3de3eff42cff4e3c2e2e42482d17a68cef64f1
crimson30 <crimson30@gmail.com>
2015-09-12 17:26:07 -0600

    Additional files.

---
+++ php/admin_tool.php
@@ -0,0 +1,153 @@
+<?php
+// PULL POST DATA
+$db_name = $_SERVER['QUERY_STRING']
+?>
+<html>
+<body>
+<table border=1 cellpadding=2>
+
+ <tr>roomname
+  <td>
+  ccz_create_db.php<br>
+  <form action="ccz_create_db.php" method="post" target="_top">
+  Room: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<INPUT size="30" name="roomname" value="<?php echo $db_name;?>"><br>
+  Hostname: <INPUT size="30" name="username">
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+  <td>
+  ccz_join.php<br>
+  <form action="ccz_join.php" method="post" target="_top">
+  Room: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<INPUT size="30" name="roomname" value="<?php echo $db_name;?>"><br>
+  Username: <INPUT size="30" name="username">
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+  <td>
+  ccz_start.php<br>
+  <form action="ccz_start.php" method="post" target="_top">
+  Room: <INPUT size="30" name="roomname" value="<?php echo $db_name;?>">
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+ </tr>
+
+ <tr>
+  <td>
+  ccz_check_start.php<br>
+  <form action="ccz_check_start.php" method="post" target="_top">
+  Room: <INPUT size="30" name="roomname" value="<?php echo $db_name;?>">
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+  <td>
+  ccz_set_bait.php<br>
+  <form action="ccz_set_bait.php" method="post" target="_top">
+  Room: &nbsp;<INPUT size="30" name="roomname" value="<?php echo $db_name;?>"><br>
+  Progress: 
+  <select name="turnprogress">
+  <option value="baitset" selected>baitset</option>
+  <option value="allresponsesin">allresponsesin</option>
+  <option value="winnerpicked">winnerpicked</option>
+  <option value="gameover">gameover</option>
+  </select>
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+  <td>
+  ccz_draw_eight.php<br>
+  <form action="ccz_draw_eight.php" method="post" target="_top">
+  Room: <INPUT size="30" name="roomname" value="<?php echo $db_name;?>">
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+ </tr>
+
+ <tr>
+  <td>
+  ccz_user_list.php<br>
+  <form action="ccz_user_list.php" method="post" target="_top">
+  Room: <INPUT size="30" name="roomname" value="<?php echo $db_name;?>"><br>
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+  <td>
+  ccz_get_bait.php<br>
+  <form action="ccz_get_bait.php" method="post" target="_top">
+  Room: <INPUT size="30" name="roomname" value="<?php echo $db_name;?>">
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+  <td>
+  ccz_get_users_responses.php<br>
+  <form action="ccz_get_users_responses.php" method="post" target="_top">
+  Room: &nbsp;<INPUT size="30" name="roomname" value="<?php echo $db_name;?>"><br>
+  Action: 
+  <select name="action">
+  <option value="waitforallfull" selected>waitforallfull</option>
+  <option value="waitforallempty">waitforallempty</option>
+  </select>
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+ </tr>
+
+ <tr>
+  <td>
+  ccz_user_submit.php<br>
+  <form action="ccz_user_submit.php" method="post" target="_top">
+  Room: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<INPUT size="30" name="roomname" value="<?php echo $db_name;?>"><br>
+  Username: &nbsp;&nbsp;<INPUT size="30" name="username"><br>
+  Submission: <INPUT size="30" name="submission">
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+  <td>
+  ccz_set_turn_progress.php<br>
+  <form action="ccz_set_turn_progress.php" method="post" target="_top">
+  Room: &nbsp;<INPUT size="30" name="roomname" value="<?php echo $db_name;?>"><br>
+  Progress: 
+  <select name="turnprogress">
+  <option value="baitset" selected>baitset</option>
+  <option value="allresponsesin">allresponsesin</option>
+  <option value="winnerpicked">winnerpicked</option>
+  <option value="gameover">gameover</option>
+  </select>
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+  <td>
+  ccz_process_winner.php<br>
+  <form action="ccz_process_winner.php" method="post" target="_top">
+  Room: &nbsp;<INPUT size="30" name="roomname" value="<?php echo $db_name;?>"><br>
+  User: 
+  <select name="user">
+  <option value="1" selected>1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+  <option value="5">5</option>
+  <option value="6">6</option>
+  <option value="7">7</option>
+  <option value="8">8</option>
+  <option value="9">9</option>
+  </select>
+  <br>
+  <input type="submit" name="submit" value="Submit" />
+  </form>
+  </td>
+ </tr>
+
+
+</table>
---
+++ php/ccz_get_users_responses.php
@@ -0,0 +1,44 @@
+<?php
+
+// LINK TO SQL
+$link = mysql_connect('localhost', 'root', 'cmsc495fall');
+if (!$link) { die('Could not connect: ' . mysql_error()); }
+
+// PULL POST DATA
+// $db_name = $_SERVER['QUERY_STRING'];
+$db_name = urlencode($_POST["roomname"]);
+$action = urlencode($_POST["action"]);
+
+
+// SELECT THAT DB
+mysql_select_db($db_name , $link) or die("Select DB Error: ".mysql_error());
+
+// NOTE: "WAIT FOR RESPONSE" is considered empty -- don't use null, because this string is displayed to dealer!
+$fullOfWAIT_FOR_RESPONSE = true; // if remains true, then is true
+$hasNoWAIT_FOR_RESPONSE = true;  // if remains true, then is true
+$response_string="";
+
+// GET USERS (do while because for mysql_feth_array gets first user)
+$tablecontents = mysql_query("SELECT * FROM users");
+if ($myrow = mysql_fetch_array($tablecontents))
+  {
+     $num = 1;
+     do
+        { $response_string=$response_string.$myrow["submission"]."|";
+          if ($myrow["submission"] == "WAIT FOR RESPONSE") { $hasNoWAIT_FOR_RESPONSE = false; }
+          if ($myrow["submission"] != "WAIT FOR RESPONSE") { $fullOfWAIT_FOR_RESPONSE = false; }
+          $num++;
+        } 
+     while ($myrow = mysql_fetch_array($tablecontents));
+  } 
+  else  { echo "User list SELECT * FROM users Error"; }
+
+if ($action == "waitforallfull" && $hasNoWAIT_FOR_RESPONSE) {echo $response_string;} else
+ if ($action == "waitforallempty" && $fullOfWAIT_FOR_RESPONSE) {echo "OK";} else
+  { echo "Submissions are neither empty nor full";}
+
+// CLOSE DATABASE
+mysql_close($link);
+
+
+?>
