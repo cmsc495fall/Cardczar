47070a83e0bcb6abb87d71541fcc8ca3aa95b582
crimson30 <crimson30@gmail.com>
2015-10-02 09:10:55 -0600

    create_db checks for previous database and deletes said database if it's an hour old or if turnprogress=gameover.

--- php/ccz_create_db.php
+++ php/ccz_create_db.php
@@ -14,65 +14,104 @@ return $out_array;
 $db_name = urlencode($_POST["roomname"]);
 $username = urlencode($_POST["username"]);
 
-// LINK TO SQL
-$link = mysqli_connect('localhost', 'root', 'cmsc495fall');
-if (!$link) { die('Could not connect: ' . mysqli_connect_error()); }
+// CHECK TO SEE IF DATABASE ALREADY EXISTS
+$temp_link = mysqli_connect('localhost', 'root', 'cmsc495fall', $db_name);
 
-// CREATE DB
-$query = 'CREATE DATABASE '.$db_name;
-mysqli_query($link, $query) or die("Create DB Error: ".mysqli_error());
+// IF DB EXISTS, DELETE AND CREATE IF OLD; IF DB DOESN'T EXIST, CREATE IT
+if ($temp_link)
+  { // IF DATABASE ALREADY EXISTS, CHECK AGE
+    // GET gamestate timecreated
+    $query = "SELECT timecreated, turnprogress FROM gamestate";
+    $result = mysqli_query($temp_link, $query);
+    $row = mysqli_fetch_assoc($result);
+    $time_created = $row[timecreated];
+    $time_since_created = time()-$time_created;
 
+    if ($time_since_created > 3600 || $row[turnprogress] == "gameover")
+      {  // DATABASE IS MORE THAN AN HOUR OLD OR TURNPROGRESS=GAMEOVER
+         // DROP DATABASE AND READY FOR ANOTHER INSTANCE
+         $query = "DROP DATABASE ".$db_name;
+         $result = mysqli_query($temp_link, $query);
+         $create_db = true;
+      } else
+      {  // DATABASE IS LESS THAN AN HOUR OLD AND NOT GAMEOVER
+         // DO NOT CREATE DB SINCE IT'S STILL ACTIVE
+         echo "Room already exists.";
+         $create_db = false;
+      }
 
-// SELECT THAT DB
-mysqli_select_db($link, $db_name) or die("Select DB Error: ".mysqli_error());
+  }
+  else // $temp_link did not work because database doesn't exist
+  { // IF DATABASE DOES NOT EXIST, OK TO CREATE A NEW ONE
+    $create_db = true;
+  }
 
-// CREATE TABLES (GAMESTATE, USERS, Q & A)
-$query = 'CREATE TABLE gamestate (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), started BOOLEAN, timecreated INT, round INT, numusers INT, dealer INT, host INT, winner INT, activebait VARCHAR(128), selectedresponse VARCHAR(128), turnprogress VARCHAR(15));';
-mysqli_query($link, $query) or die("Create gamestate Error: ".mysql_error());
-
-$query = 'CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), username VARCHAR(30) UNIQUE, points INT, submission VARCHAR(128), timelastcontact TIMESTAMP);';
-mysqli_query($link, $query) or die("Create users Error: ".mysql_error());
-
-$query = 'CREATE TABLE bait (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), text VARCHAR(128));';
-mysqli_query($link, $query) or die("Create bait Error: ".mysql_error());
-
-$query = 'CREATE TABLE responses (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), text VARCHAR(128));';
-mysqli_query($link, $query) or die("Create responses Error: ".mysql_error());
-
-
-// INSERT INTO TABLES
-$query = "INSERT INTO gamestate (started, timecreated, round, numusers, dealer, host, activebait, turnprogress) VALUES (FALSE,".time().", 0, 1, 0, 1, 'NO_BAIT_YET', 'NONE');";
-mysqli_query($link, $query) or die("gamestate INSERT Error: ".mysql_error());
-
-
-$prepared_statment = mysqli_prepare($link, "INSERT INTO users (username, points, submission) VALUES (?, ?, ?)");
-mysqli_stmt_bind_param($prepared_statment, 'sis', $u, $p, $s);
-$u = $username;
-$p = 0;
-$s = "WAIT FOR RESPONSE";
-mysqli_stmt_execute($prepared_statment);
-mysqli_stmt_close($prepared_statment);
+// CLOSE TEMP LINK
+mysqli_close($temp_link);
 
 
-$bait=file('/var/www/html/b.txt');
-shuffle($bait);
-$bait = urlencode_array($bait);
-$bait = "('" .implode("'),('",$bait)."')";
-$query = "INSERT INTO bait (text) VALUES $bait;";
-mysqli_query($link, $query) or die("Error adding bait: ".mysql_error());
+if ($create_db) {
 
-$responses=file('/var/www/html/r.txt');
-shuffle($responses);
-$responses = urlencode_array($responses);
-$responses = "('" .implode("'),('",$responses)."')";
-$query = "INSERT INTO responses (text) VALUES $responses;";
-if (mysqli_query($link, $query)) {
-    echo "OK";
-} else {
-    echo 'Error adding responses: ' . mysql_error() . "\n";
-}
+  // LINK TO SQL
+  $link = mysqli_connect('localhost', 'root', 'cmsc495fall');
+  if (!$link) { die('Could not connect: ' . mysqli_connect_error()); }
 
-// CLOSE DATABASE
-mysqli_close($link);
+  // CREATE DB
+  $query = 'CREATE DATABASE '.$db_name;
+  mysqli_query($link, $query) or die("Create DB Error: ".mysqli_error());
+
+  // SELECT THAT DB
+  mysqli_select_db($link, $db_name) or die("Select DB Error: ".mysqli_error());
+
+  // CREATE TABLES (GAMESTATE, USERS, Q & A)
+  $query = 'CREATE TABLE gamestate (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), started BOOLEAN, timecreated INT, round INT, numusers INT, dealer INT, host INT, winner INT, activebait VARCHAR(128), selectedresponse VARCHAR(128), turnprogress VARCHAR(15));';
+  mysqli_query($link, $query) or die("Create gamestate Error: ".mysql_error());
+
+  $query = 'CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), username VARCHAR(30) UNIQUE, points INT, submission VARCHAR(128), timelastcontact TIMESTAMP);';
+  mysqli_query($link, $query) or die("Create users Error: ".mysql_error());
+
+  $query = 'CREATE TABLE bait (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), text VARCHAR(128));';
+  mysqli_query($link, $query) or die("Create bait Error: ".mysql_error());
+
+  $query = 'CREATE TABLE responses (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(id), text VARCHAR(128));';
+  mysqli_query($link, $query) or die("Create responses Error: ".mysql_error());
+
+
+  // INSERT INTO TABLES
+  $query = "INSERT INTO gamestate (started, timecreated, round, numusers, dealer, host, activebait, turnprogress) VALUES (FALSE,".time().", 0, 1, 0, 1, 'NO_BAIT_YET', 'NONE');";
+  mysqli_query($link, $query) or die("gamestate INSERT Error: ".mysql_error());
+
+
+  $prepared_statment = mysqli_prepare($link, "INSERT INTO users (username, points, submission) VALUES (?, ?, ?)");
+  mysqli_stmt_bind_param($prepared_statment, 'sis', $u, $p, $s);
+  $u = $username;
+  $p = 0;
+  $s = "WAIT FOR RESPONSE";
+  mysqli_stmt_execute($prepared_statment);
+  mysqli_stmt_close($prepared_statment);
+
+
+  $bait=file('/var/www/html/b.txt');
+  shuffle($bait);
+  $bait = urlencode_array($bait);
+  $bait = "('" .implode("'),('",$bait)."')";
+  $query = "INSERT INTO bait (text) VALUES $bait;";
+  mysqli_query($link, $query) or die("Error adding bait: ".mysql_error());
+
+  $responses=file('/var/www/html/r.txt');
+  shuffle($responses);
+  $responses = urlencode_array($responses);
+  $responses = "('" .implode("'),('",$responses)."')";
+  $query = "INSERT INTO responses (text) VALUES $responses;";
+  if (mysqli_query($link, $query)) {
+      echo "OK";
+  } else {
+      echo 'Error adding responses: ' . mysql_error() . "\n";
+  }
+
+  // CLOSE LINK
+  mysqli_close($link);
+
+} // end if $create_db
 
 ?>
